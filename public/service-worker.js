1a54f2845baf27b77c75e35b3d77072e14d97d9a
nachthelad <nachthelad@gmail.com>
2023-09-30 01:04:03 -0300

    Added Service Worker & Fixed icons

---
+++ public/service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'Selfman';
+const urlsToCache = [
+  '/',
+  '/src/App.js',
+  '/src/index.js',
+  '/src/components/BottomNavigation.js',
+  '/src/components/dbOperations.js',
+  '/src/components/FavoritesComponent.js',
+  '/src/components/FavoritesPage.js',
+  '/src/components/Home',
+  '/src/components/InputForm.js',
+  '/src/components/SideBarMenu.js',
+  
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
--- src/index.js
+++ src/index.js
@@ -11,7 +11,16 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+window.addEventListener('load', () => {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+    .then((registration) => {
+      console.log('Successfully registered Service Worker:', registration);
+    })
+    .catch((error) => {
+      console.log('Service Worker registry failed:', error);
+    });
+  }
+});
+
 reportWebVitals();
